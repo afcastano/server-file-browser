7d4e0a8ae85a140688556e2a15c7a890c38f4655
afcastano <andresfelipecastano@gmail.com>
2014-08-20 01:08:00 +1000

    Refactor: now is using promises

--- app/app.js
+++ app/app.js
@@ -1,49 +1,85 @@
 var express = require('express');
 
-var fs = require('fs.extra');
+
+var qfs = require('q-io/fs');
+var Q = require('q');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var env = require(__dirname + '/../' + process.argv[2]);
 var versionProp = require(__dirname + '/../package.json');
 var async = require('async');
+var _ = require('underscore');
 
 
 var bodyParser = require('body-parser');
 
-var stats = function(filePath) {
-	var stats = fs.lstatSync(filePath); 
+function fullStat(path) {
 
-	return {
-		isDir: stats.isDirectory(),
-		size: stats.size 
-	};
+	var deferred = Q.defer();
+
+	qfs.exists(path).then(function(exists){
+		if(!exists) {
+			deferred.resolve(undefined);
+		}
+	});
+
+	qfs.stat(path).then(function(stats){
+		if(stats.isDirectory()){	
+			deferred.resolve(dirStat(path));
+		}else{
+			deferred.resolve(fileDto(undefined, path, stats));
+		}
+	}).fail(deferred.reject);
+
+	return deferred.promise;
+}
+
+function dirStat(path) {
+	var deferred = Q.defer();
+
+	listDirFiles(path, true)
+	.then(function(filesDto){
+
+		var promises = [];
+		var totalStat = {
+			size: 0,
+			isDir: true,
+			id: path
+		}
+
+		_.each(filesDto, function(fileDto){
+			if(fileDto.isDir) {
+				promises.push(dirStat(fileDto.id));
+			} else {
+				totalStat.size = totalStat.size + fileDto.size;
+			}
+		});
+
+		Q.all(promises).then(function(subDirStats){
+			_.each(subDirStats, function(subDirStat){
+				totalStat.size = totalStat.size + subDirStat.size;	
+			});
+			deferred.resolve(totalStat);
+
+		}).fail(deferred.reject);
+
+	}).fail(deferred.reject);
+
+	return deferred.promise;
 }
 
 var monitorFile = function(file, totalSize) {
 	return setInterval(function(){
-		var data = stats(file);
-		if(data.isDir) {
 
-			getDirStats(file, function (err, data){
-				
-				if (err) {
-					console.log("Error thrown on copy " + err);
-		   			io.sockets.emit('copyError',err.message);
-		   			return;
-		  		}
-
-		  		if(data === undefined) {
-		  			return;
-		  		}
-
-				console.log('Size: ' + data.size + ' files: ' + data.numberOfFiles);
-				io.sockets.emit('copyProgress', {copied: data.size, total: totalSize});	
-			});
-
-		}else {
-			io.sockets.emit('copyProgress', {copied: data.size, total: totalSize});						
-		}
+		fullStat(file)
+		.then(function(stats){
+			io.sockets.emit('copyProgress', {copied: stats.size, total: totalSize});	
+		})
+		.fail(function(err){
+			console.log("Error thrown on copy " + err.stack);
+   			io.sockets.emit('copyError',err.message);
+		});
 
 	}, 200);
 }
@@ -58,15 +94,77 @@ app.use( bodyParser.urlencoded({
 }) );
 
 
-app.get('/api/list', function(req, res){
+function listDirFiles(path, includeHidden) {
+	var deferred = Q.defer();
 
-	getContents(req.query.dir, function (content){
+	qfs.list(path)
+	.then(function(files){
 		
-			res.send(content);
+		var promises = _.chain(files)
+		.filter(function(fileName){
+
+			if(includeHidden) {
+				return true;
+			}
+
+			return isHidden(fileName);
+
+		}).map(function(fileName){
+			return loadFileDto(fileName, path);
+		}).value();
+
+		Q.all(promises).then(deferred.resolve)
+		.fail(deferred.reject);
 			
-	});
+	}).fail(deferred.reject);
+
+	return deferred.promise;
+}
+
+app.get('/api/list', function(req, res){
+	
+	var dir = req.query.dir;
+	listDirFiles(dir,false)
+	.then(function(result){
+		res.send(result);
+	})
+	.fail(function(err){
+		console.log(err);
+		res.status(500).send(err.message);	
+	});	 
 
 });
+
+function isHidden(path) {
+	var isHidden = /^\./.test(path);
+	return !isHidden;	
+}
+
+function loadFileDto(fileName, dir){
+	var deferred = Q.defer();
+
+	var fullPath = dir + '/' + fileName;
+	
+	qfs.stat(fullPath).then(function(stats){
+		deferred.resolve(fileDto(fileName, dir, stats));
+	}).fail(function(err){
+		deferred.reject(err);
+	});
+
+	return deferred.promise;
+};
+
+function fileDto(fileName, dir, stats) {
+	return {
+			label: fileName,			
+			id: dir + '/' + fileName,
+			isDir: stats.isDirectory(),
+			size: stats.size,
+			dir: dir,
+			children: []
+	}
+};
+
 
 app.get('/api/defaultpath', function(req, res) {
 	res.send({
@@ -80,230 +178,75 @@ app.get('/api/version', function(req, res){
 });
 
 app.post('/api/copy', function(req, res){
-	console.log(req.body.origin);
-	console.log(req.body.target);
-	console.log(req.body.file);
-
 	var filePath = req.body.origin + '/' + req.body.file;
 	var targetPath = req.body.target + '/' + req.body.file;
 	res.send('OK');
-	
-	try {
 
-		var data = stats(filePath);
-		console.log('start copying');
-
-		if(data.isDir){
-
-			getDirStats(filePath, function(err, dirData){
-
-				if (err) {
-					console.log("Error thrown on copy " + err);
-		   			io.sockets.emit('copyError',err.message);
-		   			return;
-		  		}
-
-				var interval = monitorFile(targetPath, dirData.size);
-				fs.copyRecursive(filePath, targetPath, function(err){
-					if (err) {
-						console.log("Error thrown on copy " + err);
-			   			io.sockets.emit('copyError',err.message);
-			   			clearInterval(interval);
-			   			return;
-			  		}
-			  		console.log('Copy dir ok!');
-			  		clearInterval(interval);
-					io.sockets.emit('copyEnd',req.body.file);	
-				});
-			});
-
-
+	fullStat(filePath).then(function(fullStats){
+		if(fullStats.isDir){
+			copyDir(filePath, targetPath, fullStats);
 		} else {
-			// setTimeout(function(){
-				var interval = monitorFile(targetPath, data.size);
-				fs.copy(filePath, targetPath, function(err){
-					if (err) {
-						console.log("Error thrown on copy " + err);
-			   			io.sockets.emit('copyError',err.message);
-			   			clearInterval(interval);
-			   			return;
-			  		}
-			  		console.log('Copy file ok!');
-			  		clearInterval(interval);
-			  		io.sockets.emit('copyEnd',req.body.file);
-						
-				});
-			// }, 5000);
-		}	
-
-	}catch(err) {
-		console.log("Error thrown on copy " + err);
-		io.sockets.emit('copyError',err.message);
-		return;
-	}
-	
-
+			copyFile(filePath, targetPath, fullStats);
+		}
+	}).fail(function(err){
+		console.log("Error thrown on copy " + err.stack);
+		io.sockets.emit('copyError', err.message);
+	});
 	
 });
 
+function copyFile(originPath, targetPath, originStats) {
+	var interval = monitorFile(targetPath, originStats.size);
+	qfs.copy(originPath, targetPath).then(function(){
+		console.log('Copy file ok!');
+  		clearInterval(interval);
+  		io.sockets.emit('copyEnd', originPath);	
+	}).fail(function(err){
+		console.log("Error thrown on copy " + err.stack);
+		io.sockets.emit('copyError',err.message);
+		clearInterval(interval);	
+	});	
+}
+
+function copyDir(originPath, targetPath, originStats) {
+	var interval = monitorFile(targetPath, originStats.size);
+	qfs.copyTree(originPath, targetPath).then(function(){
+  		console.log('Copy dir ok!');
+  		clearInterval(interval);
+		io.sockets.emit('copyEnd', originPath);	
+	}).fail(function(err){
+		console.log("Error thrown on copy " + err.stack);
+		io.sockets.emit('copyError',err.message);
+		clearInterval(interval);
+  	});
+}
+
 app.delete('/api/file', function(req, res){
-	try {
 
-		var filePath =  req.query.path;
-		
-		if(isDir(filePath)) {
-			fs.rmrf(filePath, function(err){
-				if (err) {
-					console.log("Error thrown deleting " + err);
-			 		res.status(500).send(err.message);
-					return;
-				}
-				console.log('delete dir ok!')
-				res.send('OK');
-			});
-		} else {
-			fs.unlink(filePath, function(err){
-				if (err) {
-					console.log("Error thrown deleting " + err);
-			 		res.status(500).send(err.message);
-					return;
-				}
-				console.log('delete file ok!')
-				res.send('OK');
-			});
-		}
+	var filePath =  req.query.path;
+	fullStat(filePath)
+	.then(function(stats){
 
-	}catch(err) {
-		console.log("Error thrown deleting " + err);
-		res.status(500).send(err.message);
-		return;
-	}
+		var promise = stats.isDir ? qfs.removeTree(filePath) : qfs.remove(filePath);
+
+		promise.then(function(){
+			console.log('delete ok!')
+			res.send('OK');
+		})
+		.fail(function(err){
+			console.log("Error thrown deleting " + err.stack);
+			res.status(500).send(err.message);
+		});
+
+	})
+	.fail(function(err){
+		console.log("Error thrown deleting " + err.stack);
+		res.status(500).send(err.message);	
+	});
 
 });
 
 app.use(express.static(__dirname + "/www"));
-
-function getContents(dir, cb) {
-	fs.readdir(dir,function(err, files){
-		var contents = [];
-		files = files || [];
-		for(var i = 0; i < files.length; i ++) {
-			var fileName = files[i];
-			var fullPath = dir + '/' + fileName;
-
-			var data = stats(fullPath);
-			var isHidden = /^\./.test(fileName);
-
-			if(isHidden) {
-				continue;
-			}
-
-			var fileDto = {
-				label: fileName,			
-				id: fullPath,
-				isDir: data.isDir,
-				size: data.size,
-				dir: dir,
-				children: []
-			}
-
-			contents.push(fileDto);
-		}
-
-		cb(contents);	
-	});
-}
-
-function getDirStats(dir, cb){
-	var exists = fs.existsSync(dir);
-	if(!exists) {
-		cb(undefined,undefined);
-		return;
-	}
-
-	fs.readdir(dir, function(err, files){
-
-		if(err){
-			cb(err);
-			return;	
-		}
-
-		var dirStats = {
-			id: dir,
-			numberOfFiles: 0,
-			size: 0,
-			add: function(newStats) {
-				this.numberOfFiles = this.numberOfFiles + newStats.numberOfFiles;
-				this.size = this.size + newStats.size;
-			}
-		}
-
-		var dirs = [];	
-		async.eachSeries(files, function(fileName, next){
-
-			var fullPath = dir + '/' + fileName;
-			var data = stats(fullPath);
-			var isHidden = /^\./.test(fileName);
-			if(isHidden) {
-				next();
-			}
-
-			if(data.isDir) {
-				dirs.push(fullPath);
-				next();
-			} else {
-				dirStats.numberOfFiles = dirStats.numberOfFiles + 1;
-				dirStats.size = dirStats.size + data.size;
-				next();
-			}
-
-		}, function(err){
-			if(err){
-				cb(err);
-				return;	
-			}
-		});
-
-		var totalDirs = dirs.length;
-
-		if(totalDirs == 0) {
-			cb(undefined, dirStats);
-			return;
-		}
-
-		async.each(dirs, function(curr, next){
-			getDirStats(curr, function(err, newStats){
-
-				if(err) {
-					cb(err);
-					return;
-				}
-
-				dirStats.add(newStats);
-				totalDirs = totalDirs - 1;
-
-				if(totalDirs === 0) {
-					cb(undefined, dirStats);	
-				}
-				
-			});
-
-			next();
-
-		}, function(err){
-			if(err){
-				cb(err);
-				return;
-			}
-		});
-
-		
-
-		
-
-	});
-
-}
 
 io.on('connection', function(socket){
 	console.log('A user connected');
